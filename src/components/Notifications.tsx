0073fb6532afd8b11ed68c123b4b8c10365f7f4d
hlowso <h.canderleigh@gmail.com>
2023-07-11 13:09:30 -0400

    moved notifications

---
+++ src/components/Notifications.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { SocketPayload } from 'common'
+import Marked from './Marked'
+import { useSocket, useUIContext } from '../contexts'
+
+interface NotificationProps extends SocketPayload.Notify {
+  clear: () => void
+}
+
+const Notification = ({ clear, header, body, variant }: NotificationProps) => {
+  const { Toast, Span, Div } = useUIContext()
+  return (
+    <Toast onClose={clear} bg={variant}>
+      <Span>{header}</Span>
+      <Div style={{ color: 'white' }}>
+        <Marked markdown={body} />
+      </Div>
+    </Toast>
+  )
+}
+
+const Notifications = () => {
+  const { ToastContainer } = useUIContext()
+  const { notifications, setNotifications } = useSocket()!
+  const clearNotification = (uuid: string) =>
+    setNotifications((N) => N.filter((n) => n.uuid !== uuid))
+
+  return (
+    <ToastContainer
+      position='top-end'
+      containerPosition='fixed'
+      style={{ margin: '20px' }}
+    >
+      {notifications.map((notification) => (
+        <Notification
+          key={notification.uuid}
+          {...notification}
+          clear={() => clearNotification(notification.uuid)}
+        />
+      ))}
+    </ToastContainer>
+  )
+}
+
+export default Notifications
