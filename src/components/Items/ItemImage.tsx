d1637794ddec3b31489d0b6c9600b587e1cd6088
hlowso <h.canderleigh@gmail.com>
2023-07-11 11:43:18 -0400

    moved itemimage

---
+++ src/components/Items/ItemImage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useUIContext } from '../../contexts'
+
+interface Props {
+  src: string
+  scale?: number
+}
+
+const RATIO = { W: 87, H: 70 }
+
+const ItemImage = ({ src, scale = 1 }: Props) => {
+  const { Image } = useUIContext()
+  const style = {
+    width: `${scale * RATIO.W}px`,
+    height: `${scale * RATIO.H}px`
+  }
+  return <Image src={src} style={style} />
+}
+
+export default ItemImage
