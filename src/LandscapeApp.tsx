1146c413b591566c534aa142aaca8baef5c3286d
hlowso <h.canderleigh@gmail.com>
2023-07-11 16:01:08 -0400

    moved web app to LandscapeApp

--- src/LandscapeApp.tsx
+++ src/LandscapeApp.tsx
@@ -1,3 +1,35 @@
-const LandscapeApp = () => {}
+import React from 'react'
+import Nav from './components/Nav'
+import Lists from './components/Lists'
+import Possessions from './components/Possessions'
+import { useEffect } from 'react'
+import Notifications from './components/Notifications'
+import Vendor from './components/Vendor'
+import './App.css'
+import { usePlayContext, useSocket, useUIContext } from './contexts'
+
+const LandscapeApp = () => {
+  const { Div } = useUIContext()
+  const { userId } = usePlayContext()
+  const socketEvents = useSocket()
+
+  useEffect(() => {
+    if (userId) {
+      socketEvents?.identify({ userId })
+    }
+  }, [userId, socketEvents])
+
+  return (
+    <Div className='LandscapeApp'>
+      <Nav />
+      <Div>
+        <Lists />
+        <Possessions />
+      </Div>
+      <Vendor />
+      <Notifications />
+    </Div>
+  )
+}
 
 export default LandscapeApp
