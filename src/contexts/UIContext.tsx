4eb1c07cd9142bbbe4606396418b9591dbf43bb6
hlowso <h.canderleigh@gmail.com>
2023-07-11 15:57:18 -0400

    added remaining UI components that are currently in use

--- src/contexts/UIContext.tsx
+++ src/contexts/UIContext.tsx
@@ -1,21 +1,67 @@
 import React, { createContext, PropsWithChildren, useContext } from 'react'
 import { UI } from '../types/ui-components'
 
-interface Context {
-  Div: (props: PropsWithChildren<any>) => JSX.Element | null
-  Span: (props: PropsWithChildren<any>) => JSX.Element | null
-  Image: (props: UI.ImageProps) => JSX.Element | null
-}
-
 interface Props {
   children: React.ReactNode
   components: Context
 }
 
+interface Context {
+  Div: (props: UI.DivProps) => JSX.Element | null
+  Span: (props: UI.SpanProps) => JSX.Element | null
+  P: (props: UI.PProps) => JSX.Element | null
+  ButtonGroup: (props: UI.ButtonGroupProps) => JSX.Element | null
+  Button: (props: UI.ButtonProps) => JSX.Element | null
+  Row: (props: UI.RowProps) => JSX.Element | null
+  Col: (props: UI.ColProps) => JSX.Element | null
+  Image: (props: UI.ImageProps) => JSX.Element | null
+  Link: (props: UI.LinkProps) => JSX.Element | null
+  Spinner: (props: UI.SpinnerProps) => JSX.Element | null
+  Navbar: (props: UI.NavbarProps) => JSX.Element | null
+  ToastContainer: (props: UI.ToastContainerProps) => JSX.Element | null
+  Toast: (props: UI.ToastProps) => JSX.Element | null
+  Card: (props: UI.CardProps) => JSX.Element | null
+  ListGroup: (props: UI.ListGroupProps) => JSX.Element | null
+  ListGroupItem: (props: UI.ListGroupItemProps) => JSX.Element | null
+  Accordion: (props: UI.AccordionProps) => JSX.Element | null
+  Badge: (props: UI.BadgeProps) => JSX.Element | null
+  Modal: (props: UI.ModalProps) => JSX.Element | null
+  ModalHeader: (props: UI.ModalHeaderProps) => JSX.Element | null
+  Strong: (props: UI.StrongProps) => JSX.Element | null
+  InputGroup: (props: UI.InputGroupProps) => JSX.Element | null
+  FormControl: (props: UI.FormControlProps) => JSX.Element | null
+  Icon: (props: UI.IconProps) => JSX.Element | null
+  OverlayTrigger: (props: UI.OverlayTriggerProps) => JSX.Element | null
+  Tooltip: (props: UI.TooltipProps) => JSX.Element | null
+}
+
 const UIContext = createContext<Context>({
   Div: () => null,
   Span: () => null,
-  Image: () => null
+  P: () => null,
+  ButtonGroup: () => null,
+  Button: () => null,
+  Row: () => null,
+  Col: () => null,
+  Image: () => null,
+  Link: () => null,
+  Spinner: () => null,
+  Navbar: () => null,
+  ToastContainer: () => null,
+  Toast: () => null,
+  Card: () => null,
+  ListGroup: () => null,
+  ListGroupItem: () => null,
+  Accordion: () => null,
+  Badge: () => null,
+  Modal: () => null,
+  ModalHeader: () => null,
+  Strong: () => null,
+  InputGroup: () => null,
+  FormControl: () => null,
+  Icon: () => null,
+  OverlayTrigger: () => null,
+  Tooltip: () => null
 })
 
 export const useUIContext = () => useContext(UIContext)
--- src/types/ui-components.ts
+++ src/types/ui-components.ts
@@ -1,6 +1,105 @@
+import React, { PropsWithChildren } from 'react'
+
 export namespace UI {
-  export interface ImageProps {
+  interface BaseProps {
+    key?: string | number
+    style?: React.CSSProperties
+    onClick?: React.MouseEventHandler
+    className?: string
+    title?: string
+  }
+
+  export interface DivProps extends PropsWithChildren<BaseProps> {
+    dangerouslySetInnerHTML?: { __html: TrustedHTML | string }
+  }
+
+  export interface SpanProps extends PropsWithChildren<BaseProps> {}
+
+  export interface PProps extends PropsWithChildren<BaseProps> {}
+
+  export interface ButtonGroupProps extends PropsWithChildren<BaseProps> {
+    vertical?: boolean
+  }
+
+  export interface ButtonProps extends PropsWithChildren<BaseProps> {
+    variant?: string
+    disabled?: boolean
+  }
+
+  export interface RowProps extends PropsWithChildren<BaseProps> {}
+  export interface ColProps extends PropsWithChildren<BaseProps> {}
+
+  export interface ImageProps extends BaseProps {
     src: string
-    style: React.CSSProperties
+    rounded?: boolean
+    roundedCircle?: boolean
+  }
+
+  export interface LinkProps extends PropsWithChildren<BaseProps> {
+    href: string
+  }
+
+  export interface SpinnerProps extends BaseProps {
+    small?: boolean
+  }
+
+  export interface CardProps extends PropsWithChildren<BaseProps> {
+    bg?: string
+    text?: string
+  }
+
+  export interface ListGroupProps extends PropsWithChildren<BaseProps> {}
+  export interface ListGroupItemProps extends PropsWithChildren<BaseProps> {}
+
+  export interface NavbarProps extends PropsWithChildren<BaseProps> {
+    fixed?: 'top' | 'bottom'
+    bg?: string
+  }
+
+  export interface ToastContainerProps extends PropsWithChildren<BaseProps> {
+    position: 'top-end'
+    containerPosition: 'fixed'
+  }
+
+  export interface ToastProps extends PropsWithChildren<BaseProps> {
+    onClose: () => void
+    bg: string
+  }
+
+  export interface AccordionProps extends PropsWithChildren<BaseProps> {
+    open?: boolean
+  }
+
+  export interface BadgeProps extends PropsWithChildren<BaseProps> {
+    bg: string
+  }
+
+  export interface ModalProps extends PropsWithChildren<BaseProps> {
+    show?: boolean
+    onHide?: () => void
+  }
+
+  export interface ModalHeaderProps extends PropsWithChildren<BaseProps> {}
+
+  export interface StrongProps extends PropsWithChildren<BaseProps> {}
+
+  export interface InputGroupProps extends PropsWithChildren<BaseProps> {}
+  export interface FormControlProps extends PropsWithChildren<BaseProps> {
+    value: any
+    onChange: (ev: React.ChangeEvent) => void
+  }
+
+  export interface IconProps extends BaseProps {
+    name: string
+    size?: number
+  }
+
+  export interface OverlayTriggerProps
+    extends PropsWithChildren<{ key?: string | number }> {
+    overlay: (props: any) => JSX.Element
+  }
+
+  export interface TooltipProps extends PropsWithChildren<{}> {
+    id: string
   }
 }
