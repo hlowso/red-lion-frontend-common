2b4035fe0566d9a36a6b7e8069a9008f8ddf5a94
hlowso <h.canderleigh@gmail.com>
2023-07-11 14:09:43 -0400

    moved activity modal

---
+++ src/components/Lists/ActivityModal.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ActivityRow } from 'common'
+import { useUIContext } from '../../contexts'
+
+interface Props {
+  show: boolean
+  activity?: ActivityRow
+  close: () => void
+  log: () => void
+}
+
+const ActivityModal = ({ show, activity, log, close }: Props) => {
+  const { Modal, Div, Button } = useUIContext()
+  return (
+    <Modal title={activity?.name || ''} show={show} onHide={close}>
+      <Div>
+        <p>{activity?.description}</p>
+      </Div>
+      <Div>
+        <Button onClick={close} variant='secondary'>
+          Close
+        </Button>
+        <Button onClick={log} variant='primary'>
+          Log
+        </Button>
+      </Div>
+    </Modal>
+  )
+}
+
+export default ActivityModal
