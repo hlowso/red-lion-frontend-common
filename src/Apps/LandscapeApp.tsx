5fcdc068057aeff5a76eec33d2c22b63056fd663
hlowso <h.canderleigh@gmail.com>
2023-07-12 15:29:55 -0400

    added unplanned activity logic to landscape app

--- src/Apps/LandscapeApp.tsx
+++ src/Apps/LandscapeApp.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Nav from '../components/Nav'
 import Lists from '../components/Lists'
 import Possessions from '../components/Possessions'
@@ -7,26 +7,72 @@ import Vendor from '../components/Vendor'
 import ListNav from '../components/ListNav'
 import { useUIContext, usePlayContext } from '../contexts'
 import { interfaceLists } from 'common/selectors'
-import useLists from '../hooks/useLists'
+import { ActivityPostParams, ActivityRow, ListRow } from 'common'
+import CreateActivityModal from '../components/Lists/CreateActivityModal'
+import useL from '../hooks/useLists'
+import useA from '../hooks/useActivities'
+import useT from '../hooks/useTallies'
+import { Contexts } from '..'
+import { useQueryClient } from '@tanstack/react-query'
+import SelectUnplannedActivityModal from '../components/Lists/SelectUnplannedActivityModal'
+
+const unplannedList = (lists: ListRow[]) =>
+  lists.find((l) => l.name === 'Unplanned')
+
+const listNavItems = (lists: ListRow[]) => [
+  { id: -1, name: 'Today', icon: 'BrightnessLowFill' },
+  ...interfaceLists(lists || []).map(({ id, name }) => ({
+    id,
+    name,
+    icon: 'ListCheck'
+  }))
+]
+const unplannedActivites = (lists: ListRow[], activities: ActivityRow[]) =>
+  activities.filter((a) => a.listId === unplannedList(lists)?.id)
 
 const LandscapeApp = () => {
+  const Requests = useContext(Contexts.RequestsContext)
+  const queryClient = useQueryClient()
   const { Div } = useUIContext()
-  const {
-    gameId,
-    characterId,
-    isLoading: playContextLoading
-  } = usePlayContext()
-  const { data: lists } = useLists({ gameId, characterId }, !playContextLoading)
+  const { gameId, characterId, ...context } = usePlayContext()
+  const { data: L } = useL({ gameId, characterId }, !context.isLoading)
+  const { data: A } = useA({ gameId, characterId }, !context.isLoading)
+  const { data: T } = useT({ gameId }, !context.isLoading)
+
   const [openListId, setOpenListId] = useState<number>(-1)
-  const listNavItems = [
-    { id: -1, name: 'Today', icon: 'BrightnessLowFill' },
-    ...interfaceLists(lists || []).map(({ id, name }) => ({
-      id,
-      name,
-      icon: 'ListCheck'
-    }))
-  ]
-  const [unplannedModalOpen, setUnplannedModalOpen] = useState(false)
+  const [selectUnplannedModalOpen, setSelectUnplannedModalOpen] =
+    useState(false)
+  const [createUnplannedModalOpen, setCreateUnplannedModalOpen] =
+    useState(false)
+  const [isLogging, setIsLogging] = useState(false)
+
+  const onLogUnplanned = async (activityId: number) => {
+    setIsLogging(true)
+    setSelectUnplannedModalOpen(false)
+    await Requests.completeActivityAsCharacter({
+      characterId: characterId!,
+      activityId: activityId!
+    })
+    setIsLogging(false)
+    await queryClient.invalidateQueries({ queryKey: ['games', gameId] })
+  }
+
+  const onCreateUnplanned = async (
+    params: Omit<ActivityPostParams, 'listId'>
+  ) => {
+    setIsLogging(true)
+    setCreateUnplannedModalOpen(false)
+    await Requests.createActivity({
+      ...params,
+      listId: unplannedList(L || [])?.id || -1,
+      logCompletionOnCreate: {
+        subjectId: characterId || -1,
+        subjectType: 'character'
+      }
+    })
+    setIsLogging(false)
+    await queryClient.invalidateQueries({ queryKey: ['games', gameId] })
+  }
 
   return (
     <Div className='LandscapeApp'>
@@ -42,16 +88,38 @@ const LandscapeApp = () => {
         }}
       >
         <ListNav
-          lists={listNavItems}
+          lists={listNavItems(L || [])}
           openListId={openListId}
           openList={setOpenListId}
-          openUnplannedModal={() => setUnplannedModalOpen(true)}
+          openUnplannedModal={() =>
+            unplannedActivites(L || [], A || []).length
+              ? setSelectUnplannedModalOpen(true)
+              : setCreateUnplannedModalOpen(true)
+          }
         />
-        <Lists lists={lists || []} openListId={openListId} />
+        <Lists lists={L || []} activities={A || []} openListId={openListId} />
       </Div>
       <Possessions />
       <Vendor />
       <Notifications />
+      <SelectUnplannedActivityModal
+        isLogging={isLogging}
+        show={selectUnplannedModalOpen}
+        close={() => setSelectUnplannedModalOpen(false)}
+        activities={unplannedActivites(L || [], A || [])}
+        logCompletion={onLogUnplanned}
+        createNewActivity={() => {
+          setSelectUnplannedModalOpen(false)
+          setCreateUnplannedModalOpen(true)
+        }}
+      />
+      <CreateActivityModal
+        tallies={T || []}
+        isCreating={isLogging}
+        show={createUnplannedModalOpen}
+        close={() => setCreateUnplannedModalOpen(false)}
+        createActivity={onCreateUnplanned}
+      />
     </Div>
   )
 }
