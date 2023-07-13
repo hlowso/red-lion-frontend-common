e3c23adca8e2c474ff5f05873aa154b70518e10e
hlowso <h.canderleigh@gmail.com>
2023-07-13 18:03:30 -0400

    removed onLog from Lists

--- src/components/Lists/index.tsx
+++ src/components/Lists/index.tsx
@@ -22,19 +22,9 @@ const Loading = () => {
 }
 
 const Lists = ({ lists, activities, openListId }: Props) => {
-  const queryClient = useQueryClient()
   const { Div } = useUIContext()
-  const Requests = useContext(RequestsContext)
-  const {
-    gameId,
-    characterId,
-    isLoading: playContextLoading
-  } = usePlayContext()
-
-  const [isLogging, setIsLogging] = useState(false)
   const [openActivityId, setOpenActivityId] = useState<number>()
   const openActivity = activities?.find((a) => a.id === openActivityId)
-  const isLoading = playContextLoading || isLogging
   const openListName =
     lists?.find((list) => list.id === openListId)?.name || 'Today'
   const openListActivities =
@@ -42,41 +32,16 @@ const Lists = ({ lists, activities, openListId }: Props) => {
       ? dueToday(activities || [])
       : activities?.filter((a) => a.listId === openListId)
 
-  const onLog = async () => {
-    // 1. Close modal
-    setOpenActivityId(undefined)
-
-    // 2. Set logging state
-    setIsLogging(true)
-
-    // 3. Create the log record
-    await Requests.completeActivityAsCharacter({
-      characterId: characterId!,
-      activityId: openActivityId!
-    })
-
-    // 4. Fetch updated records pertaining to current game
-    await queryClient.invalidateQueries({ queryKey: ['games', gameId] })
-
-    // 5. Done logging
-    setIsLogging(false)
-  }
-
   return (
     <Div style={{ flexGrow: 1 }}>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <ActivityList
-          listName={openListName}
-          activities={openListActivities || []}
-          openActivityModal={setOpenActivityId}
-        />
-      )}
+      <ActivityList
+        listName={openListName}
+        activities={openListActivities || []}
+        openActivityModal={setOpenActivityId}
+      />
       <ActivityModal
         show={!!openActivity}
         activity={openActivity!}
-        log={onLog}
         close={() => setOpenActivityId(undefined)}
       />
     </Div>
