26857d06560a04362b5aa3c2548e51ff9f93ff7b
hlowso <h.canderleigh@gmail.com>
2023-07-14 14:59:58 -0400

    added create activity modal to lists

--- src/components/Lists/index.tsx
+++ src/components/Lists/index.tsx
@@ -4,6 +4,7 @@ import ActivityModal from './ActivityModal'
 import ActivityList from './ActivityList'
 import { ListRow, ActivityRow } from 'common'
 import { dueToday } from 'common/selectors'
+import CreateActivityModal from './CreateActivityModal'
 
 interface Props {
   lists: ListRow[]
@@ -14,6 +15,7 @@ interface Props {
 const Lists = ({ lists, activities, openListId }: Props) => {
   const ui = useUIContext()
   const [openActivityId, setOpenActivityId] = useState<number>()
+  const [createActivityModalOpen, setCreateActivityModalOpen] = useState(false)
   const openActivity = activities?.find((a) => a.id === openActivityId)
   const openListName =
     lists?.find((list) => list.id === openListId)?.name || 'Today'
@@ -25,14 +27,21 @@ const Lists = ({ lists, activities, openListId }: Props) => {
   return (
     <ui.Div style={{ flexGrow: 1 }}>
       <ActivityList
+        showAddButton={openListId}
         listName={openListName}
         activities={openListActivities || []}
         openActivityModal={setOpenActivityId}
+        openCreateActivityModal={() => setCreateActivityModalOpen(true)}
       />
       <ActivityModal
         show={!!openActivity}
         activity={openActivity!}
         close={() => setOpenActivityId(undefined)}
+      />
+      <CreateActivityModal
+        listId={openListId}
+        show={!openListId && openListId > 0 && createActivityModalOpen}
+        close={() => setCreateActivityModalOpen(false)}
       />
     </ui.Div>
   )
