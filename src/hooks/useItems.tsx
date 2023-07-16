848dc5a24a3e1577e8267af1e4051d7be1d2fd8e
hlowso <h.canderleigh@gmail.com>
2023-07-15 23:23:09 -0400

    removed unnecessary args from hook

--- src/hooks/useItems.tsx
+++ src/hooks/useItems.tsx
@@ -1,14 +1,17 @@
 import { useContext } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { Item, ItemGetQuery } from 'common'
+import { Item } from 'common'
 import { RequestsContext } from '../contexts/RequestsContext'
+import { usePlayContext } from '../contexts'
 
-const useItems = (query: ItemGetQuery, enabled: boolean) => {
+const useItems = (query?: { id: number }) => {
   const Requests = useContext(RequestsContext)
+  const { gameId } = usePlayContext()
+
   return useQuery<Item[]>({
-    queryKey: ['games', query.gameId, 'items', query.id],
-    queryFn: () => Requests.getItems(query),
-    enabled
+    queryKey: ['games', gameId, 'items', query?.id],
+    queryFn: () => Requests.getItems({ gameId, ...query }),
+    enabled: !!gameId
   })
 }
 
