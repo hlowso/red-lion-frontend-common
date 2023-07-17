c128eee8c75dbf9cf8af0962ffdaa6d05084a929
hlowso <h.canderleigh@gmail.com>
2023-07-17 01:19:31 -0400

    app now responsible for open list and activity

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {
   FrontendContext,
   PlayProvider,
@@ -11,14 +11,26 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import LandscapeApp from './Apps/LandscapeApp'
 import PortraitApp from './Apps/PortraitApp'
 
-interface Props {
-  version: 'landscape' | 'portrait'
-}
-
 const queryClient = new QueryClient()
 
-const App = ({ version }: Props) => {
-  const { serverUrl, apiBaseUrl, components } = useContext(FrontendContext)
+export interface AppProps {
+  openListId?: number
+  setOpenListId: (id?: number) => void
+  selectUnplannedModalOpen: boolean
+  createUnplannedModalOpen: boolean
+  setSelectUnplannedModalOpen: (open: boolean) => void
+  setCreateUnplannedModalOpen: (open: boolean) => void
+}
+
+const App = () => {
+  const { serverUrl, apiBaseUrl, components, orientation } =
+    useContext(FrontendContext)
+  const [openListId, setOpenListId] = useState<number | undefined>(-1)
+  const [selectUnplannedModalOpen, setSelectUnplannedModalOpen] =
+    useState(false)
+  const [createUnplannedModalOpen, setCreateUnplannedModalOpen] =
+    useState(false)
+
   return (
     <UIProvider components={components!}>
       <RequestsProvider apiBaseUrl={apiBaseUrl!}>
@@ -26,7 +38,25 @@ const App = ({ version }: Props) => {
           <SocketProvider serverUrl={serverUrl!}>
             <PlayProvider>
               <VendorProvider>
-                {version === 'landscape' ? <LandscapeApp /> : <PortraitApp />}
+                {orientation === 'landscape' ? (
+                  <LandscapeApp
+                    openListId={openListId}
+                    setOpenListId={setOpenListId}
+                    selectUnplannedModalOpen={selectUnplannedModalOpen}
+                    createUnplannedModalOpen={createUnplannedModalOpen}
+                    setSelectUnplannedModalOpen={setSelectUnplannedModalOpen}
+                    setCreateUnplannedModalOpen={setCreateUnplannedModalOpen}
+                  />
+                ) : (
+                  <PortraitApp
+                    openListId={openListId}
+                    setOpenListId={setOpenListId}
+                    selectUnplannedModalOpen={selectUnplannedModalOpen}
+                    createUnplannedModalOpen={createUnplannedModalOpen}
+                    setSelectUnplannedModalOpen={setSelectUnplannedModalOpen}
+                    setCreateUnplannedModalOpen={setCreateUnplannedModalOpen}
+                  />
+                )}
               </VendorProvider>
             </PlayProvider>
           </SocketProvider>
