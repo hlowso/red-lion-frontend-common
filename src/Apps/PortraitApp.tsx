176d44d2177c355f2c1c2777e81f5e8834eeefe1
hlowso <h.canderleigh@gmail.com>
2023-07-17 01:19:11 -0400

    began composing Portrait app

--- src/Apps/PortraitApp.tsx
+++ src/Apps/PortraitApp.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import { useUIContext } from '../contexts'
+import { AppProps } from '../App'
+import useL from '../hooks/useLists'
+import useA from '../hooks/activities/useActivities'
+import Lists from '../components/Lists'
+import ListNav from '../components/ListNav'
+import { Util } from 'common'
 
-const PortraitApp = () => {
-  const { Div } = useUIContext()
-  return <Div>PLACEHOLDER</Div>
+const PortraitApp = ({
+  openListId,
+  setOpenListId,
+  setSelectUnplannedModalOpen,
+  setCreateUnplannedModalOpen
+}: AppProps) => {
+  const ui = useUIContext()
+  const { data: L, isLoading: LLoading } = useL()
+  const { data: A, isLoading: ALoading } = useA()
+
+  return !openListId ? (
+    <ListNav
+      lists={Util.List.listNavItems(L || [])}
+      openListId={openListId}
+      openList={(id) => setOpenListId(id)}
+      openUnplannedModal={() =>
+        Util.Activity.unplannedActivites(L || [], A || []).length
+          ? setSelectUnplannedModalOpen(true)
+          : setCreateUnplannedModalOpen(true)
+      }
+    />
+  ) : (
+    <ui.Div>
+      <ui.Button onClick={() => setOpenListId()}>Lists</ui.Button>
+      <Lists lists={L || []} activities={A || []} openListId={openListId!} />
+    </ui.Div>
+  )
 }
 
 export default PortraitApp
