6b957bd33a7420516297f6bcaf56561cdcae188c
hlowso <h.canderleigh@gmail.com>
2023-07-18 00:44:40 -0400

    displaying schedule info in activity list item

--- src/components/Lists/ActivityListItem.tsx
+++ src/components/Lists/ActivityListItem.tsx
@@ -1,33 +1,77 @@
 import React from 'react'
 import { Activity } from 'common'
 import { useUIContext } from '../../contexts'
+import { isValid } from 'common/util/date'
+import cronstrue from 'cronstrue'
 
 interface Props {
   open: () => void
   activity: Activity
 }
 
+interface Schedule {
+  late?: boolean
+  display: string
+}
+
+const getScheduleInfo = (schedule?: string): Schedule | undefined => {
+  const date = new Date(schedule!)
+  if (isValid(date)) {
+    return {
+      late: date.getTime() < new Date().getTime(),
+      display: `Due: ${date.toDateString()}`
+    }
+  }
+
+  try {
+    return { display: `Resets: ${cronstrue.toString(schedule!)}` }
+  } catch (e) {
+    return undefined
+  }
+}
+
 const ActivityListItem = ({ activity, open }: Props) => {
   const ui = useUIContext()
   const complete = (activity.countToday || 0) >= (activity.count || 1)
+  const scheduleInfo = getScheduleInfo(activity.schedule)
 
   return (
     <ui.ListGroupItem
       onClick={complete ? undefined : open}
       style={{ cursor: complete ? undefined : 'pointer' }}
     >
-      <ui.Div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <ui.Span
-          style={{
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap',
-            textDecoration: complete ? 'line-through' : undefined
-          }}
-        >
-          {activity.name}
+      <ui.Div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <ui.Div style={{ display: 'flex', flexDirection: 'column' }}>
+          <ui.Span
+            style={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              textDecoration: complete ? 'line-through' : undefined
+            }}
+          >
+            {activity.name}
+          </ui.Span>
+          {scheduleInfo && (
+            <ui.Span
+              style={{
+                fontSize: 'small',
+                color: scheduleInfo.late ? '#DC3545' : undefined
+              }}
+            >
+              {scheduleInfo.display}
+            </ui.Span>
+          )}
+        </ui.Div>
+        <ui.Span>
+          {activity.countToday} / {activity.count || 1}
         </ui.Span>
-        {activity.countToday} / {activity.count || 1}
       </ui.Div>
     </ui.ListGroupItem>
   )
