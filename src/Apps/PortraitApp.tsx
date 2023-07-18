c3898886bcd96440cddacde1a3b5afb167f2b3c2
hlowso <h.canderleigh@gmail.com>
2023-07-18 01:42:41 -0400

    added create and select unplanned modals to portrait

--- src/Apps/PortraitApp.tsx
+++ src/Apps/PortraitApp.tsx
@@ -5,6 +5,8 @@ import useL from '../hooks/useLists'
 import useA from '../hooks/activities/useActivities'
 import Lists from '../components/Lists'
 import ListNav from '../components/ListNav'
+import SelectUnplannedActivityModal from '../components/Lists/SelectUnplannedActivityModal'
+import CreateActivityModal from '../components/Lists/CreateActivityModal'
 import { unplanned } from 'common/selectors'
 import { Util } from 'common'
 
@@ -12,27 +14,50 @@ const PortraitApp = ({
   openListId,
   setOpenListId,
   setSelectUnplannedModalOpen,
-  setCreateUnplannedModalOpen
+  setCreateUnplannedModalOpen,
+  selectUnplannedModalOpen,
+  createUnplannedModalOpen
 }: AppProps) => {
   const ui = useUIContext()
   const { data: L, isLoading: LLoading } = useL()
   const { data: A, isLoading: ALoading } = useA()
 
-  return !openListId ? (
-    <ListNav
-      lists={Util.List.listNavItems(L || [])}
-      openListId={openListId}
-      openList={(id) => setOpenListId(id)}
-      openUnplannedModal={() =>
-        unplanned(L || [], A || []).length
-          ? setSelectUnplannedModalOpen(true)
-          : setCreateUnplannedModalOpen(true)
-      }
-    />
-  ) : (
+  return (
     <ui.Div>
-      <ui.Button onClick={() => setOpenListId()}>Lists</ui.Button>
-      <Lists lists={L || []} activities={A || []} openListId={openListId!} />
+      {!openListId ? (
+        <ListNav
+          lists={Util.List.listNavItems(L || [])}
+          openListId={openListId}
+          openList={(id) => setOpenListId(id)}
+          openUnplannedModal={() =>
+            unplanned(L || [], A || []).length
+              ? setSelectUnplannedModalOpen(true)
+              : setCreateUnplannedModalOpen(true)
+          }
+        />
+      ) : (
+        <ui.Div>
+          <ui.Button onClick={() => setOpenListId()}>Lists</ui.Button>
+          <Lists
+            lists={L || []}
+            activities={A || []}
+            openListId={openListId!}
+          />
+        </ui.Div>
+      )}
+      <SelectUnplannedActivityModal
+        show={selectUnplannedModalOpen}
+        close={() => setSelectUnplannedModalOpen(false)}
+        activities={unplanned(L || [], A || [])}
+        createNewActivity={() => {
+          setSelectUnplannedModalOpen(false)
+          setCreateUnplannedModalOpen(true)
+        }}
+      />
+      <CreateActivityModal
+        show={createUnplannedModalOpen}
+        close={() => setCreateUnplannedModalOpen(false)}
+      />
     </ui.Div>
   )
 }
