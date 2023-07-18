87fd74c7d397390a0ca950ee430184325bfe537a
hlowso <h.canderleigh@gmail.com>
2023-07-18 14:53:37 -0400

    type correction in useActivities

--- src/hooks/activities/useActivities.tsx
+++ src/hooks/activities/useActivities.tsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { ActivityGetParams, ActivityRow } from 'common'
+import { ActivityGetParams, Activity } from 'common'
 import { RequestsContext } from '../../contexts/RequestsContext'
 import { usePlayContext } from '../../contexts'
 
 const useActivities = (params?: ActivityGetParams) => {
   const Requests = useContext(RequestsContext)
   const { gameId, characterId } = usePlayContext()
-  return useQuery<ActivityRow[]>({
+  return useQuery<Activity[]>({
     queryKey: [
       'games',
       gameId,
