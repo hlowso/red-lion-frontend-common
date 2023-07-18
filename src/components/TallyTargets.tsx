4dd58414d9cbce7c9ce77f1ef0b30c8d470436f0
hlowso <h.canderleigh@gmail.com>
2023-07-18 02:55:30 -0400

    added overlay to progress bar

--- src/components/TallyTargets.tsx
+++ src/components/TallyTargets.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 import { useUIContext } from '../contexts'
 import useCharacterTallyTargets from '../hooks/characters/useCharacterTallyTargets'
+import { CharacterTallyTarget } from 'common'
+
+const ProgressOverlayTrigger = ({
+  id,
+  currentValue,
+  value,
+  tallyIcon,
+  children
+}: PropsWithChildren<CharacterTallyTarget>) => {
+  const ui = useUIContext()
+  const tooltip = (props: any) => (
+    <ui.Tooltip id={`target-tooltip-${id}`} {...props}>
+      <ui.Icon name={tallyIcon!} style={{ margin: '0 5px 0 0' }} />
+      <ui.Span>
+        {Math.round((currentValue || 0) / value)}% ({currentValue} / {value})
+      </ui.Span>
+    </ui.Tooltip>
+  )
+
+  return (
+    <ui.OverlayTrigger overlay={tooltip} placement='bottom'>
+      {children}
+    </ui.OverlayTrigger>
+  )
+}
 
 const TallyTargets = () => {
   const ui = useUIContext()
@@ -9,11 +34,14 @@ const TallyTargets = () => {
   return (
     <ui.Div style={{ width: '100%' }}>
       {tallyTargets?.map((target) => (
-        <ui.ProgressBar
-          key={target.id}
-          now={100 * (target.currentValue! / target.value)}
-          variant='primary'
-        />
+        <ProgressOverlayTrigger {...target}>
+          <ui.ProgressBar
+            key={target.id}
+            now={100 * (target.currentValue! / target.value)}
+            variant={target.color}
+            style={{ margin: '0 0 10px' }}
+          />
+        </ProgressOverlayTrigger>
       ))}
     </ui.Div>
   )
