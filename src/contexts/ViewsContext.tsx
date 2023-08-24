d58fa3773848d0f62f90933b5438a1da81ab8f89
hlowso <h.canderleigh@gmail.com>
2023-08-24 14:04:38 -0400

    added views context

---
+++ src/contexts/ViewsContext.tsx
@@ -0,0 +1,47 @@
+import React, {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useState
+} from 'react'
+import { Context as UIContext } from './UIContext'
+import Play from '../views/Play'
+import { AppProps } from '../App'
+import Journal from '../views/Journal'
+import { usePlayContext } from './PlayContext'
+
+type View = 'play' | 'journal'
+
+interface Props extends PropsWithChildren {}
+interface Context {
+  view: View
+  setView: (view: View) => void
+}
+
+export const ViewsContext = createContext<Context>({
+  view: 'play',
+  setView: () => {}
+})
+
+export const ViewsProvider = ({ children }: Props) => {
+  const [view, setView] = useState<View>('play')
+
+  const value = {
+    view,
+    setView
+  }
+
+  return <ViewsContext.Provider value={value}>{children}</ViewsContext.Provider>
+}
+
+export const View = (props: AppProps) => {
+  const { view } = useContext(ViewsContext)
+  const { isLoading } = usePlayContext()
+
+  switch (view) {
+    case 'play':
+      return <Play {...props} />
+    case 'journal':
+      return <Journal {...props} />
+  }
+}
