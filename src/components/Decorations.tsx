797901c3e120c0292085857f97d088cd988fb29c
hlowso <h.canderleigh@gmail.com>
2023-08-31 15:49:13 -0400

    added longest streaks to decorations

--- src/components/Decorations.tsx
+++ src/components/Decorations.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useUIContext } from '../contexts'
 import useCharacterGoals from '../hooks/characters/useCharacterGoals'
 import useActivities from '../hooks/activities/useActivities'
+import useCharacterLongestStreaks from '../hooks/characters/useCharacterLongestStreaks'
 import { Goal } from 'common/selectors'
 import { CharacterGoal } from 'common'
 
@@ -9,12 +10,15 @@ interface Props {
   openGoalModal: (id: number) => void
 }
 
-const Decoration = ({
-  id,
-  icon,
-  name,
-  onClick
-}: CharacterGoal & { onClick: () => void }) => {
+interface DecorationProps {
+  goalId?: number
+  id: number | string
+  icon: string
+  name: string
+  onClick: () => void
+}
+
+const Decoration = ({ id, icon, name, onClick }: DecorationProps) => {
   const ui = useUIContext()
 
   const tooltip = (props: any) => (
@@ -28,9 +32,11 @@ const Decoration = ({
   )
 
   return (
-    <ui.OverlayTrigger overlay={tooltip} placement='right'>
-      <ui.Icon name={icon} size={30} onClick={onClick} />
-    </ui.OverlayTrigger>
+    <ui.Div style={{ margin: '0 5px 0 0' }}>
+      <ui.OverlayTrigger overlay={tooltip} placement='right'>
+        <ui.Icon name={icon} size={30} onClick={onClick} />
+      </ui.OverlayTrigger>
+    </ui.Div>
   )
 }
 
@@ -38,20 +44,30 @@ const Decorations = ({ openGoalModal }: Props) => {
   const ui = useUIContext()
   const { data: goals, isFetching: CGFetching } = useCharacterGoals()
   const { data: activities, isFetching: AFetching } = useActivities()
-  const isFetching = CGFetching || AFetching
-  const decorations = Goal.complete(goals || [], activities || [])
+  const { data: streaks, isFetching: LSFetching } = useCharacterLongestStreaks()
+  const isFetching = CGFetching || AFetching || LSFetching
+
+  const goaldecs = Goal.complete(goals || [], activities || [])
+  const streakdecs = (streaks || [])
+    .filter((s) => s.streak >= 50)
+    .map((s) => ({
+      id: `streak-${s.activityId}`,
+      name: s.antistreak ? `Don't ${s.activityName}` : s.activityName + ' x50',
+      icon: 'Trophy'
+    }))
+
+  const decorations: DecorationProps[] = [
+    ...goaldecs.map((g) => ({ ...g, onClick: () => openGoalModal(g.goalId) })),
+    ...streakdecs.map((s) => ({ ...s, onClick: () => {} }))
+  ]
 
   return !!decorations.length ? (
     <ui.Div style={{ display: 'flex' }}>
       {isFetching ? (
         <ui.Spinner />
       ) : (
-        decorations?.map((decoration, idx) => (
-          <Decoration
-            key={`decoration-${idx}`}
-            {...decoration}
-            onClick={() => openGoalModal(decoration.goalId)}
-          />
+        decorations.map((decoration, idx) => (
+          <Decoration key={`decoration-${idx}`} {...decoration} />
         ))
       )}
     </ui.Div>
