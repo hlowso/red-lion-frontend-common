d5be050768f659430b54d48dff6b3c1b136a6cdf
hlowso <h.canderleigh@gmail.com>
2023-09-16 15:12:57 +0300

    using editing context for activities

--- src/components/Lists/index.tsx
+++ src/components/Lists/index.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { usePlayContext, useUIContext } from '../../contexts'
 import ActivityModal from './ActivityModal'
 import ActivityList from './ActivityList'
-import { ListRow, Activity, Util } from 'common'
+import { ListRow, Activity, Util, ActivityRow } from 'common'
 import { dueToday, withIdIn } from 'common/selectors'
-import EditActivityModal from './EditActivityModal'
 import useCharacterLists from '../../hooks/characters/useCharacterLists'
+import { EditingContext } from '../../contexts/EditingContext'
+import { isCron } from 'common/util/date'
 
 interface Props {
   lists: ListRow[]
@@ -17,10 +18,9 @@ interface Props {
 const Lists = ({ lists, activities, openListId, style }: Props) => {
   const { data: characterLists } = useCharacterLists()
   const { commitmentActivityIds } = usePlayContext()
+  const { edit, isEditing } = useContext(EditingContext)
   const ui = useUIContext()
   const [openActivityId, setOpenActivityId] = useState<number>()
-  const [editActivityId, setEditActivityId] = useState<number>()
-  const [editActivityModalOpen, setEditActivityModalOpen] = useState(false)
   const characterList = (characterLists || []).find(
     (cl) => cl.listId === openListId || cl.pseudoListId === openListId
   )
@@ -37,12 +37,64 @@ const Lists = ({ lists, activities, openListId, style }: Props) => {
   ).sort(Util.Activity.sort(characterList?.order || ''))
 
   const openEditActivityModal = (id?: number) => {
-    setEditActivityId(id)
-    setEditActivityModalOpen(true)
-  }
-  const closeEditActivityModal = () => {
-    setEditActivityId(undefined)
-    setEditActivityModalOpen(false)
+    const activity = activities.find((a) => a.id === id)
+    edit({
+      formName: id ? 'Edit Activity' : 'New Activity',
+      resource: 'activity',
+      fields: [
+        {
+          name: 'name',
+          kind: 'text',
+          label: 'Name',
+          value: activity?.name
+        },
+        {
+          name: 'description',
+          kind: 'textarea',
+          label: 'Description',
+          value: activity?.description
+        },
+        {
+          name: 'count',
+          kind: 'text',
+          label: 'Count',
+          value: activity?.count ? String(activity?.count) : undefined
+        },
+        {
+          name: 'schedule-date',
+          kind: 'date',
+          label: 'Schedule',
+          value: isCron(activity?.schedule!)
+            ? undefined
+            : (activity?.schedule as unknown as Date)
+        },
+        {
+          name: 'schedule-cron',
+          kind: 'text',
+          label: 'Repeats',
+          value: isCron(activity?.schedule!) ? activity!.schedule : undefined
+        }
+      ],
+      base: {
+        id,
+        listId: openListId,
+        completionDelta: {}
+      } as Partial<ActivityRow>,
+      isUpdate: !!id,
+      fieldsToPayload: (fields) => {
+        const payload = fields
+          .filter((f) => !f.name.startsWith('schedule'))
+          .reduce(
+            (payload, field) => ({ ...payload, [field.name]: field.value }),
+            {} as any
+          )
+        const date = fields.find((f) => f.name === 'schedule-date')
+        const cron = fields.find((f) => f.name === 'schedule-cron')
+
+        payload['schedule'] = cron?.value || date?.value
+        return payload
+      }
+    })
   }
 
   return (
@@ -60,15 +112,6 @@ const Lists = ({ lists, activities, openListId, style }: Props) => {
         show={!!openActivity}
         activity={openActivity!}
         close={() => setOpenActivityId(undefined)}
-      />
-      <EditActivityModal
-        listId={openListId}
-        activityId={editActivityId}
-        show={
-          editActivityModalOpen &&
-          (!!editActivityId || (!!openListId && openListId > 0))
-        }
-        close={closeEditActivityModal}
       />
     </ui.Div>
   )
